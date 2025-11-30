3e67488ced9db1d6aa51068b885bdbf4e105f508
im-rk <krramkumar2006g@gmail.com>
2025-11-30 11:32:17 +0530

    initialised repository for task and tasklist

---
+++ src/main/java/com/dev/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,13 @@
+package com.dev.tasks.repositories;
+
+import com.dev.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
+
+
